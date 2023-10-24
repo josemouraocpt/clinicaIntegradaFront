7c989db624bb9483c6f265783a2d23f02f3e2995
Leo Saes <leosaes2002@gmail.com>
2023-10-24 15:30:04 -0300

    "Trabalhe Conosco" updates

--- src/components/TrabalheImg.tsx
+++ src/components/TrabalheImg.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
-import trabalheConosco1 from "public/images/trabalheConosco1.png";
-import trabalheConosco2 from "public/images/trabalheConosco2.png";
+import trabalheConosco from "public/images/trabalheConosco.png";
 import Image from "next/image";
 import { maxHeaderSize } from "http";
 import { kMaxLength } from "buffer";
@@ -9,10 +8,8 @@ import { kMaxLength } from "buffer";
 export function TrabalheHead(){
   return(
     <div className="w-full h-80 flex relative inline-block mt-3.5">
-      <Image src={trabalheConosco1} alt="Criança deitada no chão" className="h-full w-full object-cover	drop-shadow-md rounded-s-3xl mix-blend-overlay bg-container backdrop-opacity-50	ml-5"></Image>
-      <Image src={trabalheConosco2} alt="Criança montada em cavalo" className="h-full w-full object-cover	drop-shadow-md rounded-r-3xl	mix-blend-overlay bg-container backdrop-opacity-50 mr-5"></Image>
-      <p className="absolute top-1/2 left-1/2 text-4xl text-white ml-1">CONOSCO</p>
-      <p className="absolute top-1/2 right-1/2 text-4xl text-white mr-1">TRABALHE</p>
+      <Image src={trabalheConosco} alt="Duas imagens lado a lado com filtro vermelho. A primeira com uma criança deitada em um tatame ao lado de mulher e médico. A segunda criança em cima de um cavalo com três médicos em volta." className="mx-auto"></Image>
+      <p className="absolute top-1/2 end-1/3 text-4xl text-white mr-24">TRABALHE CONOSCO</p>
     </div>
   )
 }
