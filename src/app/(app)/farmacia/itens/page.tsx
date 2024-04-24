bb586767756d98e0762a9b9751b37227c35072d2
Gontijo23 <gabgg7879@gmail.com>
2024-04-24 14:16:42 -0300

    Farmacia updates

---
+++ src/app/(app)/farmacia/itens/page.tsx
@@ -0,0 +1,10 @@
+import { ContainerFarmacia } from "@/components/ContainerFarmacia"
+
+export default function Itens(){
+    return(
+        <div>
+            <ContainerFarmacia/>
+            <h1>.</h1>
+        </div>
+    )
+}
