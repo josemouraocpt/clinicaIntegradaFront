7edd8778333b2c078063e32da0a95cf9360de114
Gontijo23 <gabgg7879@gmail.com>
2024-05-20 10:33:37 -0300

    Adicionando o cadastro de vacina

--- src/components/ContainerFarmacia.tsx
+++ src/components/ContainerFarmacia.tsx
@@ -10,15 +10,11 @@ import BackButton from "./BackButton";
 
 export function ContainerFarmacia(){
     return( 
-        <div className="p-4 space-y-7 h-screen">
+        <div className="p-4 space-y-7 ">
             <BackButton/>
 			<PageInfoText message="Você está na área da Farmácia"/>
 			<Actions icon={<TbPill size={90}/>} title="Farmácia"/>
-			<div>
-				<h2 className="font-bold text-lg mb-5">
-					Você está no setor de:{}
-                </h2>
-            </div>
+			
 		</div>
     )
 }
