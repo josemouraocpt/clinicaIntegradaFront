19e4ce9827673332f3b26e766d166a3ba6a7c4be
josemouraocpt <josemouraocpt@gmail.com>
2024-07-10 21:28:49 -0300

    teste

--- src/slices/authSlice.ts
+++ src/slices/authSlice.ts
@@ -2,7 +2,7 @@ import authService from "@/services/authService";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 //@ts-ignore
-const user = JSON.parse(localStorage.getItem("user"));
+const user = JSON.parse(localStorage.getItem("user"))
 
 const initialState = {
     user: user ? user : null,
